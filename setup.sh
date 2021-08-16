7374a137ca6f103eb5ffe21f86d8b4695bb30f20
Christopher Lester <lester@hep.phy.cam.ac.uk>
2021-08-16 19:16:42 +0100

    Config changes for testing V16 SDK

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-11.0.12.jdk/Contents/Home
+#export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-11.0.12.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-16.0.2.jdk/Contents/Home
 export PATH=$JAVA_HOME/bin:$PATH
 
